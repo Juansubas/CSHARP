ab4f8e62741b716bec91eba4afe09f3767901965
Juansubas <juansubas@gmail.com>
2023-01-10 01:05:59 -0500

    Arrays

--- Programacion/Programacion/GestionClientes.cs
+++ Programacion/Programacion/GestionClientes.cs
@@ -19,10 +19,26 @@
             listClientes.Items.Add(nombre);
             MessageBox.Show("" + listClientes.Items[0
             */
-            
 
-            
-            
+            //Array Estático
+            int[] listaDeItems = new int[] {0, 4, 2, 5 };
+
+            //Array Dinámico
+            List<int> lista = new List<int>();
+
+            lista.Add(2);
+            lista.Add(20);
+            lista.Add(15);
+            lista.Add(100);
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                int numero = lista[i];
+                MessageBox.Show(numero.ToString());
+            }
+
+
+
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
