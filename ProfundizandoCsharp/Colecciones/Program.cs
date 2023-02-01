d7f499d0f2dbbbca706be92f3b07e61b7ec139b7
Juansubas <juansubas@gmail.com>
2023-02-01 10:44:09 -0500

    List

---
+++ ProfundizandoCsharp/Colecciones/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Colecciones
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarando una Lista
+            List<int> numeros = new List<int>();
+
+            Console.WriteLine("¿Cuántos Elementos Quieres Introducir");
+
+            int elem = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nIngrese sus {elem} elementos ");
+
+            for(int i = 0; i < elem; i++)
+            {
+                numeros.Add(Int32.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine("\nImprimiendo Elementos ...\n");
+            Thread.Sleep(5000);
+
+            foreach (var i in numeros)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
