d5a316eb56f16b8553519648e95033079a6e84c7
Juansubas <juansubas@gmail.com>
2023-02-01 17:08:42 -0500

    Collections Queue

---
+++ ProfundizandoCsharp/ColeccionQueue/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColeccionQueue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numeros = new Queue<int>();
+
+            //Rellenado o agregando elementos a la cola
+
+            foreach (int numero in new int[5] { 2, 4, 6, 8, 10})
+            {
+                //Agregando
+                numeros.Enqueue(numero);
+            }
+
+            //Recorriendo la cola
+            Console.WriteLine("Recorriendo el Queue");
+
+            foreach(int numero in numeros) 
+            {
+                Console.WriteLine(numero);
+            }
+
+            //Eliminando elementos del Queue
+            Console.WriteLine("Eliminando Elementos");
+
+            //Elimina el primer en entrar
+            numeros.Dequeue();
+
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+    }
+}
