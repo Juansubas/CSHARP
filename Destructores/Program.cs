94fd41e8257d2c6e16f9ade36d172ff79f9ab06b
Juansubas <juansubas@gmail.com>
2023-02-10 14:58:10 -0500

    Destructores

---
+++ Destructores/Program.cs
@@ -0,0 +1,32 @@
+﻿ManejoArchivos miarchvo = new ManejoArchivos();
+miarchvo.mensaje();
+
+class ManejoArchivos
+{
+    StreamReader archivo = null;
+    int contador = 0;
+    string linea;
+
+    public ManejoArchivos()
+    {
+        //Flujo de datos con el archivo externo o conexión
+        archivo = new StreamReader(@"c:\texto.txt");
+
+        //Ahora vamos a leerlo
+        while(( linea = archivo.ReadLine() ) != null)
+        {
+            Console.WriteLine(linea);
+
+            contador++; 
+        }
+    }
+
+    public void mensaje()
+    {
+        Console.WriteLine("hay {0} líneas", contador);
+    }
+    ~ManejoArchivos()
+    {
+        archivo.Close();
+    }
+}
