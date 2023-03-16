49ad78416878684772626942a623c7f7eb455afe
Juan Paez <juan.paez@dms.ms>
2023-03-16 12:14:17 -0500

    Threads Synchronus

--- ProfundizandoCsharp/TrabajandoConThreads/Program.cs
+++ ProfundizandoCsharp/TrabajandoConThreads/Program.cs
@@ -8,7 +8,14 @@ class Program
         Thread t = new Thread(MetodoSaludo);
         //Ahora le indicamos que empiece
         t.Start();
+        //thread synchronize 
+        t.Join();
         //Process Synchronous
+
+        Thread t2 = new Thread(MetodoSaludo);
+        t2.Start();
+        t2.Join();
+
         Console.WriteLine("Hola alumnos desde thread (o  hilo) 1");
         //Suspends the current thread for the spicified number of milliseconds
         Thread.Sleep(1000);
